235c86bd0d6f8a1c345ff1364005c9a4abdb4fba
pssr1183 <76833054+pssr1183@users.noreply.github.com>
2022-07-19 19:53:07 +0530

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -121,3 +121,91 @@ SQL> select upper('aBC') from dual;
 UPP
 ---
 ABC
+
+
+
+SQL> select sname from sailors where sid in(select sid from reserves where bid in(select bid from boats where color=any('red','green')));
+
+SNAME
+--------------------
+Dustin
+Lubber
+Horatio
+Horatio
+
+SQL> select sname from sailors where sid not in(select sid from reserves) and age>20;
+
+SNAME
+--------------------
+Brutus
+Andy
+Rusty
+Art
+BOb
+
+
+SQL> select sname,age from sailors where sid in(select sid from reserves) or age=(select max(age) from sailors);
+
+SNAME                       AGE
+-------------------- ----------
+Dustin                       45
+Lubber                     55.5
+Horatio                      35
+Horatio                      35
+BOb                        63.5
+
+SQL> select sname,age from sailors where age in(select max(age) from sailors where sid in(select sid from reserves));
+
+SNAME                       AGE
+-------------------- ----------
+Lubber                     55.5
+
+
+SQL> select sname from sailors where sname like '%B_%b';
+
+SNAME
+--------------------
+BOb
+
+SQL> select s.sname from sailors s where not exists(select b.bid from boats b where not exists(select r.bid from reserves r where r.bid=b.bid and r.sid=s.sid));
+
+SNAME
+--------------------
+Dustin
+
+
+SQL> select * from reserves;
+
+       SID        BID DAY
+---------- ---------- ---------
+        22        101 10-OCT-98
+        22        102 10-OCT-98
+        22        103 10-AUG-98
+        22        104 10-JUL-98
+        31        102 11-OCT-98
+        31        103 11-JUN-98
+        31        104 09-MAY-98
+        64        101 09-MAY-98
+        64        102 09-MAY-98
+        74        103 09-MAY-98
+
+10 rows selected.
+
+SQL> select * from sailors;
+
+       SID SNAME                    RATING        AGE
+---------- -------------------- ---------- ----------
+        22 Dustin                        7         45
+        29 Brutus                        1         33
+        31 Lubber                        8       55.5
+        32 Andy                          8       25.5
+        58 Rusty                        10         35
+        64 Horatio                       7         35
+        74 Horatio                       9         35
+        71 Zorba                        10         16
+        85 Art                           3       25.5
+        95 BOb                           3       63.5
+
+10 rows selected.
+
+SQL>
